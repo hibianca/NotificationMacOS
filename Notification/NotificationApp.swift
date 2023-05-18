c7ff615a6e6fc8b20625098b75a15baedb0b750a
hibianca <103222620+hibianca@users.noreply.github.com>
2023-05-18 06:23:37 -0300

    Initial Commit

---
+++ Notification/NotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationApp.swift
+//  Notification
+//
+//  Created by Bianca Nathally Bezerra de Lima on 18/05/23.
+//
+
+import SwiftUI
+
+@main
+struct NotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
